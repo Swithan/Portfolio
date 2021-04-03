6a34070e1ab177c68c613fa2ca4ad8c5cc552a84
Debongnie <he201699@students.ephec.be>
2021-04-03 16:00:06 +0200

    Create react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Navbar extends React.Component {
+  render(){
+    return(
+      <div className="Navbar">
+        <a className="navbar-list" href="test">Test</a>
+      </div>
+    )
+  }
+}
+
+class Header extends React.Component {
+  render(){
+    return(
+      <header>
+        <h1>Nathan Debongnie</h1>
+        <h2>Portfolio</h2>
+        <Navbar/>
+      </header>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
     </div>
   );
 }
